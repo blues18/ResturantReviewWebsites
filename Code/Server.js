b056b3a9272c564d6eb5ce12c0a224d512a58bdd
Blue(derrick) <53116359+blues18@users.noreply.github.com>
2022-01-01 22:30:28 +0800

    1/1/2020 Restaurant(minorUpdate)
    Update Error on GetAllDetails

--- Code/Server.js
+++ Code/Server.js
@@ -9,12 +9,12 @@ var app = express();
 app.use(express.static("./public"));
 app.use(express.json());
 
-app.route('/Restauarnts').get(restuarntsController.getAllResturantDetails);//get restuarnt details
-app.route('/Reviewed').get(reviewedController.getALLReviewed);// get reviewed details
+app.route('/AllRestauarnts').get(restuarntsController.getAllResturantDetails);//get restuarnt details
+app.route('/AllReviewed').get(reviewedController.getALLReviewed);// get reviewed details
 app.route('/Reviewed').post(reviewedController.addReview)//post the reviewed Userside forgien key might need to have an ideal
 app.route('/Reviewed/:id').put(reviewedController.updateReview)//pu te updateed review comment on the id of the existing
 app.route('/Reviewed/:id').delete(reviewedController.deleteReview)//deleting the reviewed based on id
-app.route('/UserProfile').get(userprofileController.getAllUserProfile)// Get all userpofile details on sql databased
+app.route('/AllUserProfile').get(userprofileController.getAllUserProfile)// Get all userpofile details on sql databased
 app.route('/UserProfile').post(userprofileController.addUserProfile)//postto add new user 
 app.route('/UserProfile/:id').put(userprofileController.UpdateUserProfile)// put for updating the user profile details
 app.route('/UserProfile/:id').delete(userprofileController.DeleteUserProfile)// Delete the user profile details based on the existing id
--- MySql(ER)/ResturantReview.sql
+++ MySql(ER)/ResturantReview.sql
@@ -1,1 +1,14 @@
-SELECT * FROM restuarntsdb.restaurantdetails;
+INSERT INTO my_database.tutorials(title, author, SUBMISSION_DATE)
+VALUES ("Learning PHP", "John Pual", NOW());
+
+INSERT INTO my_database.tutorials(title, author,  SUBMISSION_DATE)
+VALUES ("HTML 6.0", "sally", '2020-09-22');
+
+INSERT INTO my_database.tutorials(title, author, SUBMISSION_DATE)
+VALUES ("Learning MySqL", "james", '2020-09-22');
+
+INSERT INTO my_database.tutorials(title, author,  SUBMISSION_DATE)
+VALUES ("CSS Tutorial", "daniel", '2020-09-22');
+
+INSERT INTO my_database.tutorials(title, author,  SUBMISSION_DATE)
+VALUES ("CSS Tutorial", "daniel Pual", '2020-09-22');
